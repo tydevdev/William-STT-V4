99eda5aebaa6fdf7572831e4335d75cf46dbcf31
tydevdev <typods05@gmail.com>
2025-03-31 20:02:37 -0500

    examples and aids for python interface people

---
+++ Python Interface/model_reference_example.py
@@ -0,0 +1,48 @@
+
+# Hey guys! Here's some code to reference for how to interact
+# with the trained model (or any model that is download)
+
+
+# When the app is active, you will need to have a virtual environment (venv) active which you have to initialize once 
+# and install the necessary dependencies... see below 
+# (I don't think all of these are necessary, but the "whisper" and "torch" ones are)
+
+'''
+torch==2.6.0
+numpy
+tqdm
+jiwer
+git+https://github.com/openai/whisper.git
+pysoundfile
+torchvision
+torchaudio
+transformers
+'''
+
+# in order to use this, there needs to be a folder somewhere in your project for the model and the fine-tuned model
+# these files are too big for GitHub, they are stored in our Google Drive instead. Those files are set in the .gitignore
+
+# import the appropriate libraries
+import torch
+import whisper
+
+
+def main():
+
+    # Load the small.en model architecture (this is the base, pre-trained model; it should always be called "small.en")
+    model = whisper.load_model("small.en")
+
+    # Load your fine-tuned model weights (this is the fine-tuning add-on) (the file path will need to be updated... good luck)
+    model.load_state_dict(torch.load("../models/fine_tuned/fine_tuned_whisper_small_en_v2.pth", map_location="cpu"))
+    model.eval()
+
+    # The model works by processing audio files (.wav), you will have to save the recording somewhere as a .wav and then process it
+    # Set the full path to an audio file on your Desktop (update accordingly)
+    audio_file = "/Users/tydevito/Desktop/william_s2_story.wav"
+
+    # This is the part where the model attempts the transcription of the wav file
+    result = model.transcribe(audio_file)
+    print("Transcription:", result.get("text", ""))
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -10,9 +10,16 @@ NOTES FOR ALL!!!
 https://docs.google.com/document/d/1jo3xdCb7IatSozAK4YMubMMw1vxoEOGbCJzj9PMI298/edit?usp=sharing
 
 
+Model Training: 
+I have to .gitignore the models and the audio data due to their sizes (.wav, .pt, .pth). We will store these files in the Google Drive and you will have to insert them in your folders manually. 
+https://drive.google.com/drive/folders/1RgS78DHKs9ZwxhXGVPX2nrM2u2EqhYAQ?usp=sharing
 
 
 iOS App Development:
-
 Xcode version 16.2
 "Local STT Model Runner"
+
+Python Interface:
+You guys got this. Please make the interface look nice!
+
+For integration with the model... it's actually pretty straightforward, please refer to the file called "model_reference_example" in your project folder for reference code.
